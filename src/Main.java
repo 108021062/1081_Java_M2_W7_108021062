8716222ab7d64af0b783928f8844b5707bcc2074
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 15:08:56 +0800

    排列數字大小

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int sum =0;
-        for(int i =3;i <=n;i+=3){
-            sum=sum+i;
+        Scanner sc = new Scanner(System.in);
+        float a = sc.nextFloat();
+        float Max = a;
+        float Min = a;
+        for (int i = 1; i < 10; i ++) {
+            a = sc.nextFloat();
+            if (Max < a) {
+            Max = a;
         }
-        System.out.println(sum);
+    else if (Min > a) {
+            Min = a;
+        }
+    }
+        System.out.printf("Max=%.2f\n",Max);
+        System.out.printf("Min=%.2f\n",Min);
     }
 }
