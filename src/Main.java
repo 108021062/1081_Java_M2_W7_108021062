6eedead650a1bafa87848941cdf0206b1e805135
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 16:00:05 +0800

    質數判斷

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        float a = sc.nextFloat();
-        float Max = a;
-        float Min = a;
-        for (int i = 1; i < 10; i ++) {
-            a = sc.nextFloat();
-            if (Max < a) {
-            Max = a;
-        }
-    else if (Min > a) {
-            Min = a;
+        int x = sc.nextInt();
+        int y = x/2;
+        boolean flag = true;
+        for (int i = 2; i <= y; i++) {
+            if (x % i == 0) {
+                flag = false;
+            }
+            if (flag) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            }
         }
     }
-        System.out.printf("Max=%.2f\n",Max);
-        System.out.printf("Min=%.2f\n",Min);
-    }
-}
