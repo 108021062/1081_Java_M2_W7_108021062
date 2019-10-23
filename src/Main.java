de0da95d342316d9d59394a60b7159882c8ebb0b
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-10-23 16:07:47 +0800

    N*N+1

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int y = x/2;
-        boolean flag = true;
-        for (int i = 2; i <= y; i++) {
-            if (x % i == 0) {
-                flag = false;
-            }
-            if (flag) {
-                    System.out.println("Yes");
-                } else {
-                    System.out.println("No");
-                }
-            }
+        int sum = 0;
+        for (int i=1;i<=x;i++){
+            sum=sum+i*(i+1);
         }
+        System.out.println(sum);
+
     }
+}
