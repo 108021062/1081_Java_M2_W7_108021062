112447c363461dd955c85fcfbc943a2f05fbc6c3
108021062 <108021062@live.asia.edu.tw>
2019-10-24 18:17:23 +0800

    全部變小寫

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        float x=sc.nextFloat();
-        System.out.println(9.0 * x / 5.0 + 32.0);
+        String x = sc.nextLine().toLowerCase();
+        System.out.println(x);
     }
 }
