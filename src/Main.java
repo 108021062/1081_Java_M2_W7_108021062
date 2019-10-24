d738a2f308a670710c0711632a6616b07360a7c4
108021062 <108021062@live.asia.edu.tw>
2019-10-24 18:13:47 +0800

    溫度換算

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
-        Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        System.out.println();
-        for (int r=1;r<=n;r++){
-            for (int m=1;m<=n;m++){
-                System.out.printf(" %d",r*m);
-
-            }
-            System.out.println();}
+        Scanner sc =new Scanner(System.in);
+        float x=sc.nextFloat();
+        System.out.println(9.0 * x / 5.0 + 32.0);
     }
-
 }
-
-
