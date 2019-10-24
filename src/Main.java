258400f782f485d53a2525bc8a4dcac5bb2fab46
108021062 <108021062@live.asia.edu.tw>
2019-10-24 17:03:40 +0800

    乘法表

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int x = sc.nextInt();
-        int sum = 0;
-        for (int i=1;i<=x;i++){
-            sum=sum+i*(i+1);
-        }
-        System.out.println(sum);
 
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println();
+        for (int r=1;r<=n;r++){
+            for (int m=1;m<=n;m++){
+                System.out.printf(" %d",r*m);
+
+            }
+            System.out.println();}
     }
+
 }
+
+
