77a8721b8950e01f0e5a2c74a0925b0e42011d87
108021062 <108021062@live.asia.edu.tw>
2019-10-27 18:27:49 +0800

    實心正方形

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,14 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        String x = sc.nextLine().toLowerCase();
-        System.out.println(x);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        char c = sc.next().charAt(0);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
     }
 }
