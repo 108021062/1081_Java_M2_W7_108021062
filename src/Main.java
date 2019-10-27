d04e5789f1b67f99a4e1b5924ef5fa7a67fe6363
108021062 <108021062@live.asia.edu.tw>
2019-10-27 18:35:19 +0800

    1/(2N-1

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
-        int x,y;
-        x=n;
-        y=n-1;
-        for (int i=1;i<y;y--){
-            x*=y;
+        double x=0;
+        for (double i=1;i<=n;i++){
+            x+=1/((2*i-1)*(2*i));
         }
         System.out.println(x);
         }
