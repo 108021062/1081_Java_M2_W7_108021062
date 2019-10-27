d3f9ad3d4f92123d7cb1c554e2d24cdc9d1f5193
108021062 <108021062@live.asia.edu.tw>
2019-10-27 18:46:28 +0800

    正因數

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n=sc.nextInt();
-        int x=1,y=0;
-        for (int i=1;i<=n;i++){
-            x*=2;
-            y+=x;
-        }
-        System.out.println(y);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                System.out.println(i);
         }
     }
+}
 
