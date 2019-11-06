85237ef9f982dfe6291a1f00a11f519cd4ddda87
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 15:07:07 +0800

    M2Q.17 數字總合

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,19 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        int sum=0;
-        for (int a=2;a<=x;a++){
-            int b=a;
-            while (b%2==0){
-                b/=2;
-            }
-            while (b%3==0){
-                b/=3;
-            }
-            while (b%5==0){
-                b/=5;
-            }
-            if (b==1){
-                sum=sum+a;
-            }
-        }
+       long x=sc.nextLong();
+       int sum=0;int a=0;
+       while (x>0) {
+           a++;
+           sum += x % 10;
+           x = x / 10;
+       }
+        System.out.println(a);
         System.out.println(sum);
+       }
     }
-}
+
