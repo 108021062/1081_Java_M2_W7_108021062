0f3b0a7dc23085584591587571a601dd82561abf
Yung-Chen Chou <yungchen@asia.edu.tw>
2019-11-06 14:11:35 +0800

    Q.25-1,倍數相加

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0)
-                System.out.println(i);
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int sum=0;
+        for (int a=2;a<=x;a++){
+            int b=a;
+            while (b%2==0){
+                b/=2;
+            }
+            while (b%3==0){
+                b/=3;
+            }
+            while (b%5==0){
+                b/=5;
+            }
+            if (b==1){
+                sum=sum+a;
+            }
         }
+        System.out.println(sum);
     }
 }
-
